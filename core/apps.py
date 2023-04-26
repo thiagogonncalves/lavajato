4ff5a042adfc1a856d099f1844c2d20f2e173bee
thiagogonncalves <thiago.gonncalves@gmail.com>
2023-04-25 23:07:22 -0300

    mudança de estrutura

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class OrdemservicoConfig(AppConfig):
+class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ordemservico'
+    name = 'core'
